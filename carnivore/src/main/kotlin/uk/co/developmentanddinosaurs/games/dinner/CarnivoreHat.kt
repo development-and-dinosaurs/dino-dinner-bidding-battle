3cf584bd71d0c0e095f03b4b489eb670059d55c9
Sean O'Toole <seanotoole@mail.com>
2023-06-17 18:41:00 +0100

    Add paper hat
    This is a licensed Freepik icon from FlatIcon.

--- carnivore/src/main/kotlin/uk/co/developmentanddinosaurs/games/dinner/CarnivoreHat.kt
+++ carnivore/src/main/kotlin/uk/co/developmentanddinosaurs/games/dinner/CarnivoreHat.kt
@@ -4,5 +4,5 @@ package uk.co.developmentanddinosaurs.games.dinner
  * Enumeration of hats for the carnivore players.
  */
 enum class CarnivoreHat {
-    BANANA_SKIN, CHEF_HAT, COWBOY_HAT, FOUR_LEAF_CLOVER, GRADUATION_CAP, PARTY_HAT, SANTA_HAT, SOMBRERO, TOP_HAT
+    BANANA_SKIN, CHEF_HAT, COWBOY_HAT, FOUR_LEAF_CLOVER, GRADUATION_CAP, PAPER_HAT, PARTY_HAT, SANTA_HAT, SOMBRERO, TOP_HAT
 }
