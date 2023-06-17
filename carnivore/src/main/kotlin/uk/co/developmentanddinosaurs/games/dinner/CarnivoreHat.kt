0ea4d63ec934f579e136639b3f3d0e5943bb2ac1
Sean O'Toole <seanotoole@mail.com>
2023-06-17 18:34:25 +0100

    Add sombrero
    This is a licensed Smashicons icon from FlatIcon.

--- carnivore/src/main/kotlin/uk/co/developmentanddinosaurs/games/dinner/CarnivoreHat.kt
+++ carnivore/src/main/kotlin/uk/co/developmentanddinosaurs/games/dinner/CarnivoreHat.kt
@@ -4,5 +4,5 @@ package uk.co.developmentanddinosaurs.games.dinner
  * Enumeration of hats for the carnivore players.
  */
 enum class CarnivoreHat {
-    CHEF_HAT, COWBOY_HAT, GRADUATION_CAP, PARTY_HAT, SANTA_HAT
+    CHEF_HAT, COWBOY_HAT, GRADUATION_CAP, PARTY_HAT, SANTA_HAT, SOMBRERO
 }
