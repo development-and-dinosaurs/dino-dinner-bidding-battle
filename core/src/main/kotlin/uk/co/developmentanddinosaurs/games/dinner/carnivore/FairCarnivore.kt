95204070c6a3415346aabf6e14d7f146397d2c0c
Sean O'Toole <seanotoole@mail.com>
2023-06-24 06:26:58 +0100

    Make the fair carnivore more random
    This sets a random colour (no duplicates), random hat (no duplicates), and a
    more random bid amount (possible duplicates) to the fair carnivore to make it
    a bit more interesting to watch.
    
    I think I will be updating the carnivore loader to fill out remaining spaces
    when there are not enough players with fair carnivores, so having them behave
    slightly differently is a good thing.

--- core/src/main/kotlin/uk/co/developmentanddinosaurs/games/dinner/carnivore/FairCarnivore.kt
+++ core/src/main/kotlin/uk/co/developmentanddinosaurs/games/dinner/carnivore/FairCarnivore.kt
@@ -4,6 +4,8 @@ import uk.co.developmentanddinosaurs.games.dinner.CarnivoreColour
 import uk.co.developmentanddinosaurs.games.dinner.CarnivoreHat
 import uk.co.developmentanddinosaurs.games.dinner.CraftyCodeCarnivore
 import kotlin.properties.Delegates
+import kotlin.random.Random
+import kotlin.random.nextInt
 
 /**
  * Test implementation of a [CraftyCodeCarnivore]
@@ -11,6 +13,7 @@ import kotlin.properties.Delegates
 class FairCarnivore : CraftyCodeCarnivore {
     private var playersRemaining by Delegates.notNull<Int>()
     private var meatRemaining by Delegates.notNull<Int>()
+    private val random = Random.Default
 
     override fun initialise(numberOfPlayers: Int, sizeOfMeat: Int) {
         playersRemaining = numberOfPlayers
@@ -22,9 +25,15 @@ class FairCarnivore : CraftyCodeCarnivore {
         meatRemaining -= meatEaten
     }
 
-    override fun bid(): Int = meatRemaining / playersRemaining
+    override fun bid(): Int =
+        random.nextInt(meatRemaining / (playersRemaining + 1) until meatRemaining / (playersRemaining - 1))
 
-    override fun colour(): CarnivoreColour = CarnivoreColour.ORANGE
+    override fun colour(): CarnivoreColour = colours.removeFirst()
 
-    override fun hat(): CarnivoreHat = CarnivoreHat.TOP_HAT
+    override fun hat(): CarnivoreHat = hats.removeFirst()
+
+    companion object {
+        private val colours = CarnivoreColour.values().apply { shuffle() }.toMutableList()
+        private val hats = CarnivoreHat.values().apply { shuffle() }.toMutableList()
+    }
 }
