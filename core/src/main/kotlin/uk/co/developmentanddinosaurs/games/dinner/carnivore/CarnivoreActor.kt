64a1e60d268cb73435c31a69c34da7e7ed47da65
Sean O'Toole <seanotoole@mail.com>
2023-06-28 19:26:36 +0100

    Add CarnivoreActor
    The carnivore actor is an orchestration piece that will be used as a container
    for all the other actors that are related to that carnivore. This will allow
    us to tell the carnivore what to do on a high level, like "show bid" and have
    the actual logic for how that happens live somewhere else.
    
    I don't know if it's a great idea yet. I want to separate the game logic from
    LibGDX and Scene2D, so I can run simulations without a UI, but I haven't done
    a very good job of that yet. Maybe I should have separated the game logic
    from the start and then wove it into the UI.
    
    Oh well, it'll be ok.

---
+++ core/src/main/kotlin/uk/co/developmentanddinosaurs/games/dinner/carnivore/CarnivoreActor.kt
@@ -0,0 +1,96 @@
+package uk.co.developmentanddinosaurs.games.dinner.carnivore
+
+import uk.co.developmentanddinosaurs.games.dinner.CraftyCodeCarnivore
+
+import com.badlogic.gdx.scenes.scene2d.Action
+import com.badlogic.gdx.scenes.scene2d.Actor
+import com.badlogic.gdx.scenes.scene2d.Stage
+import com.badlogic.gdx.scenes.scene2d.actions.Actions
+import com.badlogic.gdx.scenes.scene2d.actions.Actions.delay
+import com.badlogic.gdx.scenes.scene2d.actions.Actions.moveTo
+import com.badlogic.gdx.scenes.scene2d.ui.Image
+import com.badlogic.gdx.scenes.scene2d.ui.Label
+import ktx.actors.then
+
+import kotlin.properties.Delegates
+
+/**
+ * Contains all the actors related to the carnivore.
+ *
+ * Orchestrates any actions that need to happen that related to a carnivore on the stage.
+ */
+class CarnivoreActor(
+    private val codeCarnivore: CraftyCodeCarnivore,
+    private val image: Image,
+    private val hat: Image,
+    private val scroll: Image,
+    private val label: Label,
+    private val miniMeat: Image
+) : Actor() {
+    private var bid by Delegates.notNull<Int>()
+
+    /**
+     * Add carnivore actors to the stage.
+     *
+     * @param stage the stage to add actors to
+     */
+    fun addToStage(stage: Stage) {
+        stage.addActor(image)
+        stage.addActor(hat)
+        stage.addActor(scroll)
+        stage.addActor(label)
+        stage.addActor(miniMeat)
+    }
+
+    /**
+     * Have all carnivore actors act
+     */
+    override fun act(delta: Float) {
+        image.act(delta)
+        hat.act(delta)
+        scroll.act(delta)
+        label.act(delta)
+        miniMeat.act(delta)
+        super.act(delta)
+    }
+
+    /**
+     * Return the bid amount from the code carnivore
+     *
+     * @return the bid amount for the meat
+     */
+    fun bid(): Int {
+        bid = codeCarnivore.bid()
+        return bid
+    }
+
+    /**
+     * Update the code carnivore with the amount of meat eaten.
+     *
+     * @param meatEaten the amount of meat eaten this round
+     */
+    fun update(meatEaten: Int) {
+        codeCarnivore.update(meatEaten)
+    }
+
+    /**
+     * Animate the bid actors to show the carnivore bid
+     *
+     * @return the action to animate the bid actors
+     */
+    fun showBid(): Action = Actions.run {
+        label.setText(bid.toString())
+        scroll.addAction(moveTo(image.x, image.y, 1f))
+        label.addAction(moveTo(image.x, image.y, 1f))
+    }.then(delay(1f))
+
+    /**
+     * Animate the bid actors to hide the carnivore bid
+     *
+     * @return the action to hide the bid actors
+     */
+    fun hideBid(): Action = Actions.run {
+        scroll.addAction(moveTo(image.x, -150f, 1f))
+        label.addAction(moveTo(image.x, -150f, 1f))
+    }.then(delay(1f))
+}
